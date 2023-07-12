4657e22327cba94d33008a733c8f766eefd66bed
Kevin <kevinpc1997@gmail.com>
2023-07-12 12:57:22 +0530

    change helper.py to extract a more complete dataset of file extensions. Update json file in commit

--- helper.py
+++ helper.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 # get the html doc for the data i need
-doc = requests.get('https://www.computerhope.com/issues/ch001789.htm')
+doc = requests.get('https://fileinfo.com/filetypes/common')
 
 # get the beautiful soup object
 soup = BeautifulSoup(doc.text, 'html.parser')
@@ -15,17 +15,17 @@ headings = soup.find_all('h2')
 my_data = {}
 
 # iterate over all headings in the headings list (relevant ones)
-for item in headings[:-1]:
+for item in headings:
 
-    # get the unordered list that immediately follows the heading - creates a ul tag
-    list_ext = item.find_next_sibling('ul')
+    # get the div that immediately follows the heading - creates a div tag
+    list_ext = item.find_next_sibling('div')
 
-    # all the extension names are stored in bold tags. hence to find all instances of "b" in the ul tag
-    exts = list_ext.find_all('b')
+    # all the extension names are stored in a tags. hence to find all instances of "a" in the div tag
+    exts = list_ext.find_all('a')
 
     # create my data keys - with the first word of the heading, with the keys as a list of extension names stored
     # as strings
-    my_data[item.contents[0].split(' ')[0]] = [ext.text for ext in exts]
+    my_data[item.contents[0]] = [ext.text.lower() for ext in exts]
 
 # writing a json file
 with open('extensions.json', 'w') as extensions:
